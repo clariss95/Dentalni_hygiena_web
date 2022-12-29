e8961c3e70f90744b2873d846565d91b805425d6
Klára Šteflová <klara.steflova@gmail.com>
2022-12-29 13:49:42 -0330

    responsive design

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+
+function initMap() {
+    var location = { lat: 49.306770, lng: 14.147090 };
+    var map = new google.maps.Map(document.getElementById("map"), { zoom: 18, center: location });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+
+
+}
